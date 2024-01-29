ed9ea4ed5fa54b5e911b5520caf75653503b01d9
Henning Holm <git@henningholm.de>
2024-01-29 21:38:09 +0100

    Add function running axum web server

---
+++ examples/run_web_server.py
@@ -0,0 +1,8 @@
+from pyo3_demo import run_web_server
+
+def main():
+    print("Running axum web server...")
+    run_web_server()
+
+if __name__ == "__main__":
+    main()
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use axum::{response::Html, routing::get, Router};
 use pyo3::prelude::*;
 
 /// Formats the sum of two numbers as string.
@@ -6,9 +7,34 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+// Set up and run axum web service
+async fn web_app() {
+    // build our application with a route
+    let app = Router::new().route("/", get(handler));
+
+    // run it
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
+        .await
+        .unwrap();
+    println!("Listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
+}
+
+/// Run axum web server in Tokio runtime
+#[pyfunction]
+fn run_web_server() {
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    rt.block_on(web_app())
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn pyo3_demo(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(run_web_server, m)?)?;
     Ok(())
 }
