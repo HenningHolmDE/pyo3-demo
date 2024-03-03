5aa828dde1c6a5bd52e68b8b06aa0159d292bef2
Henning Holm <git@henningholm.de>
2024-03-03 11:42:41 +0100

    Switch web service back to axum/tokio
    This allows for using a asynchronous web framework, opening the path
    for non-blocking operation of the web service.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
+use std::{process::abort, thread};
+
+use axum::{extract::State, response::Html, routing::get, Router};
 use pyo3::prelude::*;
-use tiny_http::{Response, Server};
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -7,24 +9,56 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
-/// Run tiny-http web server
-#[pyfunction]
-fn run_web_server(py: Python, callback: PyObject) {
-    let server = Server::http("127.0.0.1:8000").unwrap();
+/// App state accessible by handlers
+#[derive(Clone, Debug)]
+struct AppState {
+    callback: PyObject,
+}
 
-    for request in server.incoming_requests() {
-        let response = Response::from_string(
-            callback
-                .call0(py)
-                .expect("callback not callable")
-                .to_string(),
-        )
-        .with_header(tiny_http::Header {
-            field: "Content-Type".parse().unwrap(),
-            value: "text/html; charset=utf8".parse().unwrap(),
-        });
-        request.respond(response).unwrap();
+/// Example handler calling Python callback function
+async fn handler(State(state): State<AppState>) -> Html<String> {
+    Html(Python::with_gil(|py| {
+        state
+            .callback
+            .call0(py)
+            .expect("callback not callable")
+            .to_string()
+    }))
+}
+
+/// Set up and run axum web service
+async fn web_app(callback: PyObject) {
+    // prepare app state to be accessed by handler functions
+    let state = AppState { callback };
+
+    // build our application with a route
+    let app = Router::new().route("/", get(handler)).with_state(state);
+
+    // run it
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
+        .await
+        .unwrap();
+    println!("Listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+/// Run axum web server in Tokio runtime
+#[pyfunction]
+fn run_web_server(py: Python, callback: PyObject) -> PyResult<()> {
+    let asyncio = py.import("asyncio")?;
+    let event_loop = asyncio.call_method0("new_event_loop")?;
+    asyncio.call_method1("set_event_loop", (event_loop,))?;
+
+    thread::spawn(move || {
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        rt.block_on(web_app(callback))
+    });
+
+    let event_loop = (*event_loop).call_method0("run_forever");
+    if event_loop.is_err() {
+        abort();
     }
+    Ok(())
 }
 
 /// A Python module implemented in Rust.
