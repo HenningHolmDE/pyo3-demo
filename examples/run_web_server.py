b21e61b800b70d534477a8ceb6525b83475c1897
Henning Holm <git@henningholm.de>
2024-03-04 16:28:50 +0100

    Add JSON handlers

--- examples/run_web_server.py
+++ examples/run_web_server.py
@@ -1,20 +1,28 @@
 from datetime import datetime
+import json
 import time
 from pyo3_demo import WebServer
 
 
-class MyClass:
-    pass
-
-
-def html_handler():
+def html_callback() -> str:
     current_time = datetime.now().strftime("%H:%M:%S")
     return f"<h1>Hello from Python!</h1>\nThe current time is: <b>{current_time}</b>"
 
 
+def get_json_callback() -> str:
+    json_data = {"current_time": datetime.now().strftime("%H:%M:%S")}
+    return json.dumps(json_data)
+
+
+def post_json_callback(body) -> str:
+    json_data = json.loads(body)
+    json_data["current_time"] = datetime.now().strftime("%H:%M:%S")
+    return json.dumps(json_data)
+
+
 def main():
     print("Running web server...")
-    web_server = WebServer(html_handler)
+    web_server = WebServer(html_callback, get_json_callback, post_json_callback)
     web_server.start()
     for i in reversed(range(1, 6)):
         print(f"Shutting down in {i} ...")
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 use std::thread;
 
-use axum::{extract::State, response::Html, routing::get, Router};
+use axum::{
+    extract::State,
+    response::Html,
+    routing::{get, post},
+    Json, Router,
+};
 use pyo3::{exceptions::PyRuntimeError, prelude::*};
 use tokio::sync::oneshot;
 
@@ -13,27 +18,60 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 /// App state accessible by handlers
 #[derive(Clone, Debug)]
 struct AppState {
-    callback: PyObject,
+    html_callback: PyObject,
+    get_json_callback: PyObject,
+    post_json_callback: PyObject,
 }
 
-/// Example handler calling Python callback function
-async fn handler(State(state): State<AppState>) -> Html<String> {
+/// Example handlers calling Python callback functions
+async fn html_handler(State(state): State<AppState>) -> Html<String> {
     Html(Python::with_gil(|py| {
         state
-            .callback
+            .html_callback
             .call0(py)
-            .expect("callback not callable")
+            .expect("html_handler not callable")
+            .to_string()
+    }))
+}
+async fn get_json_handler(State(state): State<AppState>) -> Json<String> {
+    Json(Python::with_gil(|py| {
+        state
+            .get_json_callback
+            .call0(py)
+            .expect("get_json_handler not callable")
+            .to_string()
+    }))
+}
+async fn post_json_handler(State(state): State<AppState>, body: String) -> Json<String> {
+    Json(Python::with_gil(|py| {
+        state
+            .post_json_callback
+            .call1(py, (body,))
+            .expect("get_json_handler not callable")
             .to_string()
     }))
 }
 
 /// Set up and run axum web service
-async fn web_app(callback: PyObject, shutdown_rx: oneshot::Receiver<()>) {
+async fn web_app(
+    html_callback: PyObject,
+    get_json_callback: PyObject,
+    post_json_callback: PyObject,
+    shutdown_rx: oneshot::Receiver<()>,
+) {
     // prepare app state to be accessed by handler functions
-    let state = AppState { callback };
+    let state = AppState {
+        html_callback,
+        get_json_callback,
+        post_json_callback,
+    };
 
     // build our application with a route
-    let app = Router::new().route("/", get(handler)).with_state(state);
+    let app = Router::new()
+        .route("/", get(html_handler))
+        .route("/get_json", get(get_json_handler))
+        .route("/post_json", post(post_json_handler))
+        .with_state(state);
 
     // run it
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
@@ -52,16 +90,24 @@ async fn web_app(callback: PyObject, shutdown_rx: oneshot::Receiver<()>) {
 /// Start/stop wrapper for the web server
 #[pyclass]
 struct WebServer {
-    handler: PyObject,
+    html_callback: PyObject,
+    get_json_callback: PyObject,
+    post_json_callback: PyObject,
     shutdown_tx: Option<oneshot::Sender<()>>,
 }
 
 #[pymethods]
 impl WebServer {
     #[new]
-    fn py_new(handler: PyObject) -> Self {
+    fn py_new(
+        html_callback: PyObject,
+        get_json_callback: PyObject,
+        post_json_callback: PyObject,
+    ) -> Self {
         Self {
-            handler,
+            html_callback,
+            get_json_callback,
+            post_json_callback,
             shutdown_tx: None,
         }
     }
@@ -70,14 +116,21 @@ impl WebServer {
         if self.shutdown_tx.is_some() {
             return Err(PyRuntimeError::new_err("Web server already running"));
         }
-        let handler_clone = self.handler.clone();
+        let html_callback = self.html_callback.clone();
+        let get_json_callback = self.get_json_callback.clone();
+        let post_json_callback = self.post_json_callback.clone();
 
         let (tx, rx) = oneshot::channel::<()>();
         self.shutdown_tx = Some(tx);
 
         thread::spawn(move || {
             let rt = tokio::runtime::Runtime::new().unwrap();
-            rt.block_on(web_app(handler_clone, rx))
+            rt.block_on(web_app(
+                html_callback,
+                get_json_callback,
+                post_json_callback,
+                rx,
+            ))
         });
         Ok(())
     }
